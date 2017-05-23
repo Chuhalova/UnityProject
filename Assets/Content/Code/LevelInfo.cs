99902dd8ddfd4c18730347b9ed16877cc63bcc99
Veronika <its.hard.to.do.original.email@gmail.com>
2017-05-24 01:21:27 +0300

    Add project

---
+++ Assets/Content/Code/LevelInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelInfo : MonoBehaviour {
+
+
+	public static LevelInfo current;
+
+	void Awake()
+	{
+		current = this;
+	}
+
+	Vector3 startingPosition;
+	public void setStartPosition(Vector3 pos)
+	{
+		this.startingPosition = pos;
+	}
+	public void onRabbitDeath(Rabbit rabbit)
+	{
+		rabbit.transform.position = this.startingPosition;
+	}
+}
---
+++ Assets/Content/Code/deathZone.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deathZone : MonoBehaviour{
+
+
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+			Rabbit rabbit = collider.GetComponent<Rabbit>();
+
+			if (rabbit != null)
+		{
+			
+			LevelInfo.current.onRabbitDeath(rabbit);
+		}
+	}
+}
