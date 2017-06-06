8f3d6c118c8737ecb69aeb479514253db2839e4b
Veronika <its.hard.to.do.original.email@gmail.com>
2017-06-06 22:50:38 +0300

    main menu added , button 'play' added  , button settings added

---
+++ Assets/buttonMainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine.Events;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class buttonMainMenu : MonoBehaviour
+{
+//	public buttonMainMenu playButton;
+//	public UnityEvent signalOnClick = new UnityEvent();
+//	void Start()
+//	{
+//		playButton.signalOnClick.AddListener(this._onClick);
+//	}
+	public void _onClick()
+	{
+		SceneManager.LoadScene ("Level");
+	}
+}
