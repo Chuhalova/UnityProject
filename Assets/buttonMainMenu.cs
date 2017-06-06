2f06b44befbe916d890bf4059ea5179c931ea97d
Veronika <its.hard.to.do.original.email@gmail.com>
2017-06-06 23:49:38 +0300

    scene 'choose level added' with rabbit , which has animations and coins counter

--- Assets/buttonMainMenu.cs
+++ Assets/buttonMainMenu.cs
@@ -12,6 +12,6 @@ public class buttonMainMenu : MonoBehaviour
 //	}
 	public void _onClick()
 	{
-		SceneManager.LoadScene ("Level");
+		SceneManager.LoadScene ("ChooseLevel");
 	}
 }
---
+++ Assets/door1.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine.Events;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class door1 : MonoBehaviour
+{
+
+
+	void OnCollisionEnter2D(Collision2D col){
+		Collider2D collider = col.collider;
+		if(col.transform.tag == "Player"){
+			SceneManager.LoadScene ("Level");
+	}
+}
+}
